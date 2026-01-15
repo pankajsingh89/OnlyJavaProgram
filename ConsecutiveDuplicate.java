04e8a38854738037d31cdfb693f7c4a797292c09
Your Name <your.email@example.com>
2026-01-16 05:22:47 +0530

    Consecutive program

---
+++ ConsecutiveDuplicate.java
@@ -0,0 +1,28 @@
+//  WAP to find the longest consecutive alphabetical substring from a given string
+
+class ConsecutiveDuplicate{
+
+    public static void main(String[] args){
+
+        String str="abcdeffghijjklmnooppqrsttuuvwxxyz";  //input string
+        String longest="";
+        String current=""+str.charAt(0);
+
+        for(int i=1;i<str.length();i++){
+            if(str.charAt(i)==(str.charAt(i-1)+1)){
+                current+=str.charAt(i);
+            }else{
+                if(current.length()>longest.length()){
+                    longest=current;
+                }
+                current=""+str.charAt(i);
+            }
+        }
+        if(current.length()>longest.length()){
+            longest=current;
+        }
+        System.out.println(longest);
+    }
+
+
+}
